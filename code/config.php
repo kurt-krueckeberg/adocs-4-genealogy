6caaba99d80c1ce7474e0e880a4e3f4867179e3b
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2025-07-15 09:27:30 -0400

    $1

--- code/config.php
+++ code/config.php
@@ -1,24 +1,50 @@
 <?php
 
-$config = array(
-'output_folder' => "/home/kurt/adocs-4-genealogy/m/timelines/p/',
-'timelines' => array( 'output_file' => 'jobst-heinrich-1767-timeline.adoc',
-                      'page_header' => "= Jobst Heinrich Krückeberg, born 1765, Timeline\n:page-role: doc-width\n",
-                      'input_folder' => "/home/kurt/adocs-4-genealogy/m/petzen/p/",
-                      'input_files' => array(
-                      "petzen-band1a-image211.adoc",
-                      "petzen-band1a-image319.adoc",
-                      "petzen-band2-image5-3.adoc",
-                      "petzen-band2-image55.adoc",
-                      "petzen-band2-image70.adoc",
-                      "petzen-band2-image239.adoc",
-                      "petzen-band2-image81.adoc",
-                      "petzen-band2-image198.adoc",
-                      "petzen-band2-image91.adoc",
-                      "petzen-band2-image96.adoc",
-                      "petzen-band2-image207-1.adoc",
-                      "petzen-band2-image27.adoc",
-                      "petzen-band2-image314.adoc")
-                      )
-);
+$config =  [
+'output_folder' => '/home/kurt/adocs-4-genealogy/m/timelines/p',
+'timelines' => 
+  [
+    [
+    'output_file'  => '/home/kurt/adocs-4-genealogy/m/timelines/p/johann-heinrich-k-1744.adoc',
+    'page_header'  => "= Johann Heinrich Kückeberg [1767-1805] Timeline\n:page-role: doc-width\n",
+    'input_folder' => '/home/kurt/adocs-4-genealogy/m/petzen/p/',
+    'input_files'  => [
+                      "petzen-band1a-image220.adoc",
+                      "petzen-band1a-image320.adoc",
+                      "petzen-band2-image5-1.adoc",
+                      "petzen-band2-image52.adoc",
+                      "petzen-band2-image59-60.adoc",
+                      "petzen-band2-image230.adoc",
+                      "petzen-band2-image71.adoc",
+                      "petzen-band2-image82-1.adoc",
+                      "petzen-band2-image243.adoc",
+                      "petzen-band2-image201.adoc",
+                      "petzen-band2-image207-2.adoc",
+                      "petzen-band2-image339.adoc",
+                      "petzen-band2-image211-3.adoc",
+                      "petzen-band2-image348.adoc",
+                      ],
+    ],
+    [
+    'output_file'  => 'jobst-heinrich-1767-timeline.adoc',
+    'page_header'  => "= Jobst Heinrich Krückeberg, born 1765, Timeline\n:page-role: doc-width\n", 
+    'input_folder' => "/home/kurt/adocs-4-genealogy/m/petzen/p/",
+    'input_files'  => [
+                     'petzen-band1a-image211.adoc',
+                     'petzen-band1a-image319.adoc',
+                     'petzen-band2-image5-3.adoc',
+                     'petzen-band2-image55.adoc',
+                     'petzen-band2-image70.adoc',
+                     'petzen-band2-image239.adoc',
+                     'petzen-band2-image81.adoc',
+                     'petzen-band2-image198.adoc',
+                     'petzen-band2-image91.adoc',
+                     'petzen-band2-image96.adoc',
+                     'petzen-band2-image207-1.adoc',
+                     'petzen-band2-image27.adoc',
+                     'petzen-band2-image314.adoc'
+                     ]
+    ]
+  ]
+];
 
---
+++ code/main.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+function goto_regex(\SplFileObject $file, string $regex) : void
+{ 
+   foreach($file as $line) {
+            
+       if (preg_match( $regex, $line) === 1)   
+           break;
+   }
+}
+
+class TimelineCreator {
+
+   private \SplFileObject $ofile;
+
+   public function __construct(string $outfile, string $header)
+   {
+      $this->ofile = new \SplFileObject($outfile, "w");
+
+      $this->ofile->fwrite($header . "\n");
+   }
+
+   public function __invoke(string $fname)
+   {
+      $file = new \SplFileObject($fname, "r");
+      
+      $header = $file->fgets();
+
+      $this->ofile->fwrite("=" . $header);
+      
+      goto_regex($file, "@^== Family Relationship@");
+      
+      while (!$file->eof()) { // foreach would call rewind()!
+      
+         $line = $file->fgets();
+          
+         // We exit when a line starts with "== "
+         $rc = preg_match("/^== /", $line); 
+      
+         if ($rc === 1) 
+             break;
+      
+          $this->ofile->fwrite($line);
+      }
+   }
+}
+
+$config['output_folder'];
+
+foreach ($timelines as $timeline) {
+
+        $creator = new TimelineCreator($config['output_folder'] . $timeline['output_file'], $timeline['page_header']);
+        
+        $input_folder = $timeline['input_folder'];
+        
+        foreach($time_line['input_files'] as $file) {
+         
+           $creator($input_folder . $file);  
+        }
+}
