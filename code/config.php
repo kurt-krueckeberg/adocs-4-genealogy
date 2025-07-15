66b0453a88ab12e89e7a7e0f44af80d59e0f8870
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2025-07-15 07:43:17 -0400

    $1

---
+++ code/config.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = array(
+'output_folder' => "/home/kurt/adocs-4-genealogy/m/timelines/p/',
+'timelines' => array( 'output_file' => 'jobst-heinrich-1767-timeline.adoc',
+                      'page_header' => "= Jobst Heinrich Krückeberg, born ?, Timeline\n:page-role: doc-width\n",
+                      'input_folder' => "/home/kurt/adocs-4-genealogy/m/petzen/p/",
+                      'input_files' => array(
+                      "petzen-band1a-image211.adoc",
+                      "petzen-band1a-image319.adoc",
+                      "petzen-band2-image5-3.adoc",
+                      "petzen-band2-image55.adoc",
+                      "petzen-band2-image70.adoc",
+                      "petzen-band2-image239.adoc",
+                      "petzen-band2-image81.adoc",
+                      "petzen-band2-image198.adoc",
+                      "petzen-band2-image91.adoc",
+                      "petzen-band2-image96.adoc",
+                      "petzen-band2-image207-1.adoc",
+                      "petzen-band2-image27.adoc",
+                      "petzen-band2-image314.adoc")
+                      )
+);
+
