1cda5966eb47568311f80d4b607791031d07e2f2
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2025-07-16 15:13:22 -0400

    $1

--- code/make-timeslines.php
+++ code/make-timeslines.php
@@ -13,21 +13,30 @@ function goto_regex(\SplFileObject $file, string $regex) : void
 class TimelineCreator {
 
    private \SplFileObject $ofile;
+   private string $module;
 
-   public function __construct(string $outfile, string $header)
+   public function __construct(string $outfile, string $header, string $module)
    {      
       $this->ofile = new \SplFileObject($outfile, "w");
 
       $this->ofile->fwrite($header . "\n");
+
+      $this->module = $module;
    }
 
-   public function __invoke(string $fname)
+   public function __invoke(string $input_folder, string $fname)
    {
-      $file = new \SplFileObject($fname, "r");
+      $file = new \SplFileObject($input_folder . $fname, "r");
       
-      $header = $file->fgets();
+      $line = $file->fgets();
 
-      $this->ofile->fwrite("=" . $header);
+      $line =  rtrim($line); // remove newline and whitespace from right.
+
+      $header = substr($line,2); // skip over "= ".
+     
+      $xref = "== xref:" . $this->module . ":" . $fname . '[' . $header . ']';
+
+      $this->ofile->fwrite($xref);
       
       goto_regex($file, "@^== Family Relationship@");
       
@@ -37,14 +46,14 @@ class TimelineCreator {
           
          // We exit when a line starts with "== "
          $rc = preg_match("/^== /", $line);  
-         
-         // Convert any "xref:petzen-" to be "xref:petzen:petzen-"
-         $line = preg_replace('@(xref):(petzen-)@', "$1:petzen:$2", $line);
-      
+              
          if ($rc === 1) 
              break;
-      
-          $this->ofile->fwrite($line);
+
+          // Convert any "xref:petzen-" to be "xref:petzen:petzen-"
+         $line = preg_replace('@(xref):(petzen-)@', "$1:petzen:$2", $line);
+
+         $this->ofile->fwrite($line);
       }
    }
 }
@@ -53,12 +62,12 @@ $config = \yaml_parse_file('./config.yml');
 
 foreach ($config['timelines'] as $timeline) {
     
-        $creator = new TimelineCreator($config['output_folder'] . '/' . $timeline['output_file'], $timeline['page_header']);
+        $creator = new TimelineCreator($config['output_folder'] . '/' . $timeline['output_file'], $timeline['page_header'], $timeline['module']);
         
-        $input_folder = $timeline['input_folder'];
+        $input_folder = $config['input_folder'] . '/modules/' . $timeline['module'] . '/pages/';
         
         foreach($timeline['input_files'] as $file) {
          
-           $creator($input_folder . $file);  
+           $creator($input_folder, $file);  
         }
 }
